84ccc11b670bdfc9f3f6208eff600ae40d65f0fa
OlehKulyk2710 <92692410+OlehKulyk2710@users.noreply.github.com>
2022-04-24 00:03:16 +0300

    Task-03: Done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listOfIngredientsRef = document.querySelector('[id="ingredients"]');
+
+const arrOfIngredientItems = ingredients.map(item => {
+  const tagLi = document.createElement('li');
+  tagLi.classList.add('item');
+  tagLi.textContent = item;
+
+  return tagLi;
+});
+
+listOfIngredientsRef.append(...arrOfIngredientItems);
